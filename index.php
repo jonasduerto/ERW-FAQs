de6f270a874e3a8cd60378549e5fdc48a873d63c
Jonás Duerto <jonasduerto@users.noreply.github.com>
2018-05-16 18:45:55 -0400

    Update

--- index.php
+++ index.php
@@ -13,14 +13,15 @@
 require_once('faq_items.php');
 require_once('faq_category.php');
 
-add_shortcode( 'faqs', function ( $atts ){
+
+add_shortcode( 'faqs', function ( $atts = array() ){
     extract( shortcode_atts( array(
         'catname'       => '',
         'show_cont'     => 'yes',
         'show_sn_share' => 'yes',
         'show_R'        => 'yes',
         'style'         => '',
-        'col'           => false,
+        'full'          => false,
         'show_num'      => false,
         'bs'            => 3,
     ), $atts ) );
@@ -31,42 +32,52 @@ add_shortcode( 'faqs', function ( $atts ){
         'orderby'   => 'order',
         'showposts' => '-1'
     ));
-    $i        = 0;
-    $row      = $col ? 'col-1' : 'col-2';
-    $count    = count($loop->posts);
-    $dct      = $count/2 ;
-    $output   = '<div class="faqs">';
-        if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); $i++;
-            if ($count = $dct) {
-                $output .= '<div class="col-md-5 col-md-offset-1">';
-            } else {
-                $output .= '<div class="col-md-6">';
-            }
-            if ($show_num) {
-                $output .= '<div class="listFaq '.$row.'">';
-                $output .= '    <div class="panel-body">';
-                $output .= '        <span class="redon">'. $i .'</span><h4 class="panel-title question">'. get_the_title() .'</h4>';
-                $output .= '        <p class="answer"><span class="resp">'.__( 'A:', 'themeFAQs' ).' </span>'. do_shortcode(get_the_content()) .'</p>';
-                $output .= '    </div>';
-                $output .= '</div>';
-            } else {
-                if ($i == $dct+1) {
-                    $output .= '  </div>';
+
+    $row          = $full ? 'full' : '';
+    $count        = count($loop->posts);
+    $dct          = $count/2 ;
+    $i            = 1;
+    $count        = 0;
+
+    ob_start(); ?>
+
+    <div class="faqs row justify-content-center align-content-center">
+        <?php if ( $loop->have_posts() ) : 
+            while ( $loop->have_posts() ) : $loop->the_post();
+                $_show_cont = ($show_cont == 'yes' ) ? '<span class="redon">'. $i .') </span>' : '' ;
+                $_show_R    = ($show_R    == 'yes' ) ? '<span class="resp">R: </span>'         : '' ;
+                
+                if ($count = $dct) {
+                    $output .= '<div class="col-md-5 col-md-offset-1">';
+                } else {
                     $output .= '<div class="col-md-6">';
                 }
-                $output .= '  <div class="animated fadeInLeft wow" data-wow-delay=".'. $i .'s">';
-                $output .= '    <div class="question-q-box">'.__( 'Q.', 'themeFAQs' ).'</div>';
-                $output .= '    <h4 class="question">'. get_the_title() .'</h4>';
-                $output .= '    <p class="answer">' . do_shortcode(get_the_content()) . '</p>';
-                $output .= '  </div>';
-            }
-            $output .= '</div>';
+                if ($show_num) {
+                    $output .= '<div class="listFaq '.$row.'">';
+                    $output .= '    <div class="panel-body">';
+                    $output .=          '<h4 class="panel-title question">'. $_show_cont . get_the_title() .'</h4>';
+                    $output .= '        <p class="answer">'. $_show_R; . do_shortcode(get_the_content()) .'</p>';
+                    $output .= '    </div>';
+                    $output .= '</div>';
+                } else {
+                    if ($i == $dct+1) {
+                        $output .= '  </div>';
+                        $output .= '<div class="col-md-6">';
+                    }
+                    $output .= '  <div class="animated fadeInLeft wow" data-wow-delay=".'. $i .'s">';
+                    $output .= '    <div class="question-q-box">'.__( 'Q.', 'themeFAQs' ).'</div>';
+                    $output .= '    <h4 class="question">'. get_the_title() .'</h4>';
+                    $output .= '    <p class="answer">' . do_shortcode(get_the_content()) . '</p>';
+                    $output .= '  </div>';
+                }
+                $output .= '</div>';
 
-        endwhile;
-    //    wp_reset_postdata();
-        endif;
-    $output .= '</div>';
+           $i++; endwhile; ?>
+        <?php endif; ?>
 
+    </div>
+
+    <?php $output .= ob_get_clean();
     return $output;
 });
 
